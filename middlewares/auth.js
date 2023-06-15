09c35fdf5b5e5e799af37fba6188521cb04d8dde
abrehan2 <rehannajam2@gmail.com>
2023-06-15 21:09:41 +0500

    Created a role and authentication functions to manage user access

---
+++ middlewares/auth.js
@@ -0,0 +1,33 @@
+// IMPORTS -
+const ErrorHandler = require("../utils/errorHandler");
+const AsynErr = require("./asyncErr");
+const jwt = require("jsonwebtoken");
+const userModel = require("../models/userModel");
+
+// AUTHENTICATE USER -
+exports.isAuthUser = AsynErr(async (req, res, next) => {
+  const { token } = req.cookies;
+  if (!token) {
+    return next(new ErrorHandler("Please login to access", 401));
+  }
+
+  const decodeData = jwt.verify(token, process.env.JWT_SECRET);
+  req.user = await userModel.findById(decodeData.id);
+
+  next();
+});
+
+// CHECK ROLES -
+exports.authorizeRoles = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new ErrorHandler(
+          `Role: ${req.user.role} is not allowed to access this resource`,
+          403
+        )
+      );
+    }
+    next();
+  };
+};
