429d47c5e6fef4e2303df5aecad7c997febed910
abrehan2 <rehannajam2@gmail.com>
2023-06-15 21:15:05 +0500

    Created a function to generate a token and store it in a cookie

---
+++ utils/jwtToken.js
@@ -0,0 +1,20 @@
+// CREATE A TOKEN AND SAVE IT IN COOKIE -
+const sendToken = (user, statusCode, res) => {
+  const token = user.generateJwt();
+
+  const options = {
+    expires: new Date(
+      Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+    ),
+
+    httpOnly: true,
+  };
+
+  res.status(statusCode).cookie("token", token, options).json({
+    success: true,
+    token,
+    user,
+  });
+};
+
+module.exports = sendToken;
